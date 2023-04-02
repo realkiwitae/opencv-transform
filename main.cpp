34cab5d49dcff8a2e99f5afc4779f78ef7761f36
KiwiTae <adrien@movensee.com>
2023-04-02 15:43:42 +0200

    Save filter output to files

--- main.cpp
+++ main.cpp
@@ -1,28 +1,50 @@
 #include <iostream>
 #include <cmath>
+#include <vector>
 #include <opencv2/opencv.hpp>
 
 using namespace std;
 using namespace cv;
-Mat rotate(Mat array, double angle_in_degrees);
-Mat rotate_and_translate(Mat array, double angle_in_degrees , int cx , int cy);
-Mat rotate_and_translate2(Mat array, double angle_in_degrees , int cx , int cy);
+
+Mat rotate(const Mat array, double angle_in_degrees);
+Mat rotate_and_translate(const Mat array, double angle_in_degrees , int cx , int cy);
+Mat rotate_and_translate2(const Mat array, double angle_in_degrees , int cx , int cy);
 
 int main() {
     Mat image = imread("./media/lena.png");
-    Mat rotated_image = rotate_and_translate2(image, 45,image.cols/2,image.rows/2);
-    imwrite("./media/output.jpg", rotated_image);
+    Mat result;
+
+    // create a vector ofimages
+    vector<Mat> images = {
+        image,
+        rotate(image, 45),
+        rotate_and_translate(image, 45,image.cols/2,image.rows/2),
+        rotate_and_translate2(image, 45,image.cols/2,image.rows/2)
+    };
+
+    // concatenate the images horizontally
+    hconcat(images, result);
+
+    // display the concatenated image
+    imshow("Lena - My function - Rotated&Translated - Rotated&Translated&Wrapped", result);
+    waitKey(0);
+    imwrite("./media/output.jpg", result);
+    imwrite("./media/myfunction.jpg", images[1]);
+    imwrite("./media/rotated_translated.jpg", images[2]);
+    imwrite("./media/rotated_translated_wrapped.jpg", images[3]);
+
+
     return 0;
 }
 
 
-Mat rotate(Mat array, double angle_in_degrees)
+Mat rotate(const Mat array, double angle_in_degrees)
 {
     // rotate image with angle . Center of the rotation is topleft corner.
 
     int h = array.rows;
     int w = array.cols;
-    Mat result(h, w, array.type());
+    Mat result = Mat::zeros(h, w, array.type());
     double angle = angle_in_degrees * M_PI / 180;
     Mat rotation_matrix = (Mat_<double>(2,2) << cos(angle), sin(angle), -sin(angle), cos(angle));
 
@@ -42,13 +64,13 @@ Mat rotate(Mat array, double angle_in_degrees)
     return result;
 }
 
-Mat rotate_and_translate(Mat array, double angle_in_degrees , int cx , int cy)
+Mat rotate_and_translate(const Mat array, double angle_in_degrees , int cx , int cy)
 {
     // rotate image with angle . Center of the rotation is topleft corner.
 
     int h = array.rows;
     int w = array.cols;
-    Mat result(h, w, array.type());
+    Mat result = Mat::zeros(h, w, array.type());
     double angle = angle_in_degrees * M_PI / 180;
     Mat rotation_matrix = (Mat_<double>(3,3) << cos(angle), sin(angle), 0,-sin(angle), cos(angle),0,0,0,1);
     Mat translation_matrix = (Mat_<double>(3,3) << 1, 0, -cx,0,1,-cy,0,0,1);
@@ -69,7 +91,7 @@ Mat rotate_and_translate(Mat array, double angle_in_degrees , int cx , int cy)
     return result;
 }
 
-Mat rotate_and_translate2(Mat array, double angle_in_degrees , int cx , int cy)
+Mat rotate_and_translate2(const Mat array, double angle_in_degrees , int cx , int cy)
 {
     // rotate image with angle . Center of the rotation is topleft corner.
 
